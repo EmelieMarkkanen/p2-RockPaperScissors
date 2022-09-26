8dd8bb4af62cf52cfcba328362d9ed373ae94606
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2022-09-26 10:52:20 +0000

    Add functions, styling and text/buttons
    After suggestion from school mentor added functions for closing rules popup, added popup for reset game question, added increment score function, with styling and text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
-const computerChoiceDisplay = document.getElementById("computer-choice");
-const playerChoiceDisplay = document.getElementById("player-choice");
-const resultDisplay = document.getElementById("result");
-const possibleChoices = document.querySelectorAll(".button");
+// Global variables 
+
+let computerChoiceDisplay = document.getElementById("computer-choice");
+let playerChoiceDisplay = document.getElementById("player-choice");
+let resultDisplay = document.getElementById("result");
+let possibleChoices = document.querySelectorAll(".button");
+
 let playerChoice;
 let computerChoice;
 let result;
 
+let win = "You win";
+let lose = "You lose";
+let draw = "It's a draw";
 
 //Event listener
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  playerChoice = e.target.id;
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
+  playerChoice = event.target.id;
   playerChoiceDisplay.innerHTML = playerChoice;
   generateComputerChoice();
   getResult();
@@ -46,56 +52,53 @@ function generateComputerChoice() {
 // Generate result for winner/loser and update score
 
 function getResult() {
-  let win = "You win";
-  let lose = "You lose";
-  let draw = "It's a draw";
 
   if (computerChoice === playerChoice) {
     result = draw;
     drawScore();
+    incrementScore(result);
   } else if (computerChoice === "Rock") {
     if (playerChoice === "Paper" || playerChoice === "Spock") {
       result = win;
-      playerScore();
+      incrementScore(result);
     } else {
       result = lose;
-      computerScore();
+      incrementScore(result);
     }
   } else if (computerChoice === "Paper") {
     if (playerChoice === "Lizard" || playerChoice === "Scissors") {
       result = win;
-      playerScore();
+      incrementScore(result);
     } else {
       result = lose;
-      computerScore();
+      incrementScore(result);
     }
   } else if (computerChoice === "Scissors") {
     if (playerChoice === "Rock" || playerChoice === "Spock") {
       result = win;
-      playerScore();
+      incrementScore(result);
     } else {
       result = lose;
-      computerScore();
+      incrementScore(result);
     }
   } else if (computerChoice === "Lizard") {
     if (playerChoice === "Scissors" || playerChoice === "Rock") {
       result = win;
-      playerScore();
+      incrementScore(result);
     } else {
       result = lose;
-      computerScore();
+      incrementScore(result);
     }
   } else if (computerChoice === "Spock") {
     if (playerChoice === "Lizard" || playerChoice === "Paper") {
       result = win;
-      playerScore();
+      incrementScore(result);
     } else {
       result = lose;
-      computerScore();
+      incrementScore(result);
     }
-
   }
-  resultDisplay.innerHTML = result;
+    resultDisplay.innerHTML = result;
 }
 
 //Generate and update score count for player, computer and draw
@@ -115,6 +118,28 @@ function drawScore() {
   document.getElementById("draw-score").innerText = ++yourScore;
 }
 
+function incrementScore(result) {
+  if (result = win) {
+     playerScore()
+  } else if (result = lose) {
+     computerScore();
+  } else {
+     drawScore()
+  }
+    resultDisplay.innerHTML = result;
+}
+
+//Show reset game question
+
+function toggleResetGame() {
+  let reset = document.getElementById("X");
+  if (reset.style.display === "block") {
+    reset.style.display = "none";
+  } else {
+    reset.style.display = "block";
+  }
+}
+
 //Reset score count to start over
 
 function resetGame() {
@@ -124,15 +149,19 @@ function resetGame() {
   document.getElementById("player-choice").innerText = "";
   document.getElementById("computer-choice").innerText = "";
   document.getElementById("result").innerText = "";
+  playerChoice = null;
+  computerChoice = null;
+  result = null;
 }
 
 // Hide/show rules 
 
-function rules() {
-  var x = document.getElementById("rules");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+function toggleRules() {
+  let rules = document.getElementById("rules");
+  if (rules.style.display === "block") {
+    rules.style.display = "none";
   } else {
-    x.style.display = "block";
+    rules.style.display = "block";
   }
 }
+
